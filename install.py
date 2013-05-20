7e6f459f723ebd3cddd7d9cb17767404ffae6969
Ben Kelly <ben.kelly@ursys.com.au>
2013-05-20 11:14:23 +1000

    minor update to finding dotfiles dir

--- install.py
+++ install.py
@@ -10,7 +10,7 @@ HOME_DIR = os.path.expanduser("~")
 if len(sys.argv) > 1: HOME_DIR = sys.argv[1]
 
 def find_valid_files():
-    ls = os.listdir(".")
+    ls = os.listdir(os.path.join(HOME_DIR, "dotfiles"))
     new_list = []
     for f in ls:
         if len(f) > len(SUFFIX) and f[-len(SUFFIX):] == ".symlink":
