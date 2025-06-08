93fc7957895887ba387e841dd07886b26d819cde
Ben Kelly <ben@benjii.net>
2025-06-08 21:13:12 +0000

    Add nvim-neo-tree

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -835,6 +835,17 @@ require('lazy').setup({
     end,
   },
 
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    }
+  },
+
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- put them in the right spots if you want.
